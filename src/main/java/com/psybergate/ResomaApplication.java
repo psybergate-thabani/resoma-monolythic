2557c40cd63f71497b159648015144c6937ac132
unknown <moses@psybergate.co.za>
2020-04-29 12:35:57 +0200

    Added initial spring boot project to the master repository

---
+++ src/main/java/com/psybergate/ResomaApplication.java
@@ -0,0 +1,13 @@
+package com.psybergate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResomaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psybergate/ResomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psybergate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
